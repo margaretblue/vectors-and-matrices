dea0b489d46482ff36974bee389e440fb13f011f
MARGARET MORRIS <margaretblue@MARGARETs-MacBook-Pro.local>
2015-07-06 15:58:03 -0700

    added vector_add() vector_subtract()

--- matrix_math.py
+++ matrix_math.py
@@ -20,25 +20,49 @@ def shape(vector_or_matrix):
     number of rows (for a vector) or the number of rows and columns
     (for a matrix.)"""
     #if a vector, return (#rows,)
-    # C = [[1, 2],
+    # matrixxx = [[1, 2],
     #       [2, 1],
     #       [1, 2]]
     # should return (3,2)
+    # vectorrr = [3,2]
+    # should return (2,)
     if type(vector_or_matrix[0]) is int:
         return ((len(vector_or_matrix)),)
     if type(vector_or_matrix[0]) is list:
         return ((len(vector_or_matrix),len(vector_or_matrix[0])))
 
 
-def vector_add():
-    pass
+def vector_add(first_vector, second_vector):
+    """
+    [a b]  + [c d]  = [a+c b+d]
+
+    Matrix + Matrix = Matrix
+    v = [1, 3, 0]
+    w = [0, 2, 4]
+    vector_add(v, w) == [1, 5, 4]
+    """
+    idx = 0
+    added_vector = []
+    print(first_vector)
+    for num in first_vector:
+        added_vector.append((first_vector[idx] + second_vector[idx]))
+        idx += 1
+    return added_vector
 
 
 def vector_add_is_communicative():
     pass
 
 
-def vector_sub():
+def vector_sub(first_vector, second_vector):
+    idx = 0
+    added_vector = []
+    print(first_vector)
+    for num in first_vector:
+        added_vector.append((first_vector[idx] - second_vector[idx]))
+        idx +=1
+    return added_vector
+
     pass
 
 
@@ -109,4 +133,6 @@ if __name__ == '__main__':
     print(shape([3,4]))
     print(type(shape([3,4])))
     print(shape([[1,2,3],[2,3,4],[5,6,7]]))
+    print(vector_add([1,2,3],[5,6,7]))
+    print(vector_sub([1,2,3],[5,6,7]))
 
