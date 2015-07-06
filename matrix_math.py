55bc9964e873c9a5548bb7a0142fac4ab3eda59c
MARGARET MORRIS <margaretblue@MARGARETs-MacBook-Pro.local>
2015-07-06 12:56:53 -0700

    added skeleton of defs

--- matrix_math.py
+++ matrix_math.py
@@ -1,2 +1,84 @@
 class ShapeException(Exception):
     pass
+
+
+def shape_vectors():
+    pass
+
+
+def vector_add():
+    pass
+
+
+def vector_add_is_communicative():
+    pass
+
+
+def vector_sub():
+    pass
+
+
+def vector_sub_checks_shapes():
+    pass
+
+
+def vector_sum():
+    pass
+
+
+def vector_sum_checks_shapes():
+    pass
+
+
+def test_dot():
+    pass
+
+
+def dot_checks_shapes():
+    pass
+
+
+def vector_multiply():
+    pass
+
+
+def vector_mean():
+    pass
+
+
+def magnitude():
+    pass
+
+
+def shape_matrices():
+    pass
+
+
+def matrix_row():
+    pass
+
+
+def matrix_col():
+    pass
+
+
+def matrix_scalar_multiply():
+    pass
+
+
+def matrix_vector_multiply():
+    pass
+
+
+def matrix_vector_multiply_checks_shapes():
+    pass
+
+
+def matrix_matrix_multiply():
+    pass
+
+
+def matrix_matrix_multiply_checks_shapes():
+    pass
+
+
